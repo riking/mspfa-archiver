37068a7f52f301cf52c3cbe574d3480ca310d8c4
Kane York <kyork@student.42.us.org>
2018-02-22 09:43:12 -0800

    add script to launch chrome with --allow-file-access

---
+++ launch_chrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CHROME_ARGS="--user-data-dir=/tmp/localChrome" "--allow-file-access-from-files"
+
+if [ -d /Applications/Google\ Chrome.app/ ]; then
+	CHROME_BINARY="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+elif true; then
+	# todo
+	true
+fi
+
+"$CHROME_BINARY" $CHROME_ARGS
